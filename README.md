ed0b3de192a26cf488ca79e4b422f8d69ad41d69
Fabricio Pombo Koch <fabriciokoch@yahoo.com.br>
2016-09-01 21:09:02 -0300

    adding mvc controller

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # AspnetcoreAngular
 A simple CRUD project using asp.net core and angularjs
 
-Create a new project called "WebApp" and choose an empty template:
+STEP-BY-STEP
+Open your Visual Studio 2015 (make sure you have asp.net core installed) and create a new project called "WebApp" and choose an empty template:
 ![New Project](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image1.png "New Project")
 
 Run the project and you should see a "Hello World!" on your browser.
@@ -9,3 +10,14 @@ Run the project and you should see a "Hello World!" on your browser.
 Now let's add a MVC Controller to serve our initial page. We're gonna use this page for our Single Page Application (SPA).
 After adding MVC dependencies our project.json should be like this:
 ![MVC Dependencies](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image2.png "MVC Dependencies")
+
+Modify your startup.cs to add the MVC funcionality:
+![add mvc](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image3.png "add mvc")
+![use mvc](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image4.png "use mvc")
+
+After that, create a MVC Controller called "HomeController" inside a new folder called "Controllers": 
+![new mvc controller](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image5.png "new mvc controller")
+![homecontroller](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image6.png "homecontroller")
+
+Now, we need to create a MVC View Page for the Index action of the HomeController. This way, create a new folder "Views/Home" and add a new MVC View Page into the created folder:
+![new mvc view](https://github.com/fabriciokoch/AspnetcoreAngular/blob/master/docs/images/Image7.png "new mvc view")
--- src/WebApp/src/WebApp/Startup.cs
+++ src/WebApp/src/WebApp/Startup.cs
@@ -8,30 +8,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace WebApp
-{
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+namespace WebApp {
+  public class Startup {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+      app.UseStaticFiles();
+      app.UseMvc(routes => {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+      });
+
+      loggerFactory.AddConsole();
+
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      }
+    }
+  }
 }
